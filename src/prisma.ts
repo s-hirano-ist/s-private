954de1d03e5145ae03ebf36d591bbb78046bb3b2
s-hirano-ist <s-hirano-ist@outlook.com>
2024-12-31 14:04:10 +0900

    refactor: 💡 prisma connection on dev

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,11 +1,22 @@
 // In case of error of prisma on development environment
 // https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
-
+import { env } from "@/env.mjs";
 import { PrismaClient } from "@prisma/client";
 // import { PrismaClient } from '@prisma/client/edge' // FIXME: for edge
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient({
-	omit: { users: { password: true } },
-}).$extends(withAccelerate());
+const prismaClientSingleton = () => {
+	return new PrismaClient({ omit: { users: { password: true } } }).$extends(
+		withAccelerate(),
+	);
+};
+// biome-ignore lint:
+declare const globalThis: {
+	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
 export default prisma;
+
+if (env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
