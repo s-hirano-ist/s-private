168fd438181a51c83a6c7b892c47b1f0cccb1282
s-hirano-ist <s-hirano-ist@outlook.com>
2025-01-13 12:23:29 +0900

    refactor: 💡 add error handling

--- src/app/(viewer)/viewer/@contents/page.tsx
+++ src/app/(viewer)/viewer/@contents/page.tsx
@@ -1,8 +1,11 @@
+import { StatusCodeView } from "@/components/card/status-code-view";
 import { Unauthorized } from "@/components/card/unauthorized";
 import { Badge } from "@/components/ui/badge";
+import { ERROR_MESSAGES } from "@/constants";
 import { checkSelfAuthOrRedirectToAuth } from "@/features/auth/utils/get-session";
 import { hasContentsPermission } from "@/features/auth/utils/role";
 import { ViewerStack } from "@/features/viewer/components/viewer-stack";
+import { loggerError } from "@/pino";
 import prisma from "@/prisma";
 
 const path = "contents";
@@ -10,26 +13,42 @@ const path = "contents";
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-	await checkSelfAuthOrRedirectToAuth();
+	try {
+		await checkSelfAuthOrRedirectToAuth();
 
-	const hasAdminPermission = await hasContentsPermission();
+		const hasAdminPermission = await hasContentsPermission();
 
-	const images = await prisma.staticContents.findMany({
-		select: { title: true, uint8ArrayImage: true },
-	});
+		const images = await prisma.staticContents.findMany({
+			select: { title: true, uint8ArrayImage: true },
+		});
 
-	return (
-		<>
-			{hasAdminPermission ? (
-				<>
-					<Badge className="m-2 flex justify-center">
-						コンテンツ数: {images.length}
-					</Badge>
-					<ViewerStack path={path} images={images} imageType="svg" />
-				</>
-			) : (
-				<Unauthorized />
-			)}
-		</>
-	);
+		return (
+			<>
+				{hasAdminPermission ? (
+					<>
+						<Badge className="m-2 flex justify-center">
+							コンテンツ数: {images.length}
+						</Badge>
+						<ViewerStack path={path} images={images} imageType="svg" />
+					</>
+				) : (
+					<Unauthorized />
+				)}
+			</>
+		);
+	} catch (error) {
+		loggerError(
+			ERROR_MESSAGES.UNEXPECTED,
+			{
+				caller: "ContentsPage",
+				status: 500,
+			},
+			error,
+		);
+		return (
+			<div className="flex flex-col items-center">
+				<StatusCodeView statusCode="500" />
+			</div>
+		);
+	}
 }
--- src/features/image/components/all-image-stack.tsx
+++ src/features/image/components/all-image-stack.tsx
@@ -1,7 +1,9 @@
+import { StatusCodeView } from "@/components/card/status-code-view";
 import { ImageStack } from "@/components/stack/image-stack";
-import { PAGE_SIZE } from "@/constants";
+import { ERROR_MESSAGES, PAGE_SIZE } from "@/constants";
 import { getUserId } from "@/features/auth/utils/get-session";
 import { generateUrlWithMetadata } from "@/features/image/actions/generate-url-with-metadata";
+import { loggerError } from "@/pino";
 import prisma from "@/prisma";
 
 type Props = {
@@ -9,30 +11,46 @@ type Props = {
 };
 
 export async function AllImageStack({ page }: Props) {
-	const userId = await getUserId();
+	try {
+		const userId = await getUserId();
 
-	const _images = await prisma.images.findMany({
-		where: { userId },
-		select: {
-			id: true,
-		},
-		orderBy: { id: "desc" },
-		skip: (page - 1) * PAGE_SIZE,
-		take: PAGE_SIZE,
-	});
+		const _images = await prisma.images.findMany({
+			where: { userId },
+			select: {
+				id: true,
+			},
+			orderBy: { id: "desc" },
+			skip: (page - 1) * PAGE_SIZE,
+			take: PAGE_SIZE,
+		});
 
-	const images = await Promise.all(
-		_images.map(async (image) => {
-			const response = await generateUrlWithMetadata(image.id);
-			if (!response.success) return { src: "/not-found.png" };
+		const images = await Promise.all(
+			_images.map(async (image) => {
+				const response = await generateUrlWithMetadata(image.id);
+				if (!response.success) return { src: "/not-found.png" };
 
-			return {
-				src: response.data.url,
-				width: response.data.metadata.width,
-				height: response.data.metadata.height,
-			};
-		}),
-	);
+				return {
+					src: response.data.url,
+					width: response.data.metadata.width,
+					height: response.data.metadata.height,
+				};
+			}),
+		);
 
-	return <ImageStack data={images} />;
+		return <ImageStack data={images} />;
+	} catch (error) {
+		loggerError(
+			ERROR_MESSAGES.UNEXPECTED,
+			{
+				caller: "AllImagePage",
+				status: 500,
+			},
+			error,
+		);
+		return (
+			<div className="flex flex-col items-center">
+				<StatusCodeView statusCode="500" />
+			</div>
+		);
+	}
 }
