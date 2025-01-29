632a52a57eb5bed0374d12246aa9ec00494fce45
s-hirano-ist <s-hirano-ist@outlook.com>
2025-01-30 08:10:26 +0900

    fix: 🐛 schema type of static images

--- prisma/migrations/20250129231002_add_static_image_table/migration.sql
+++ prisma/migrations/20250129231002_add_static_image_table/migration.sql
@@ -1,11 +1,8 @@
 -- CreateTable
 CREATE TABLE "st_images" (
     "id" TEXT NOT NULL,
-    "status" "Status" NOT NULL DEFAULT 'UNEXPORTED',
-    "user_id" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "deleted_at" TIMESTAMP(3),
     "content_type" TEXT NOT NULL,
     "file_size" INTEGER,
     "width" INTEGER,
