572f56f0116bd2a0c01712a311acf8ff17559979
s-hirano-ist <s-hirano-ist@outlook.com>
2025-02-03 21:12:36 +0900

    fix: 🐛 design of image skeleteleton

--- src/components/stack/image-stack-skeleton.tsx
+++ src/components/stack/image-stack-skeleton.tsx
@@ -3,9 +3,9 @@ import { SKELETON_STACK_SIZE } from "@/constants";
 
 export function ImageStackSkeleton() {
 	return (
-		<div className="grid grid-cols-1 gap-2 p-4 sm:grid-cols-2 sm:gap-4">
+		<div className="grid grid-cols-4 gap-2 p-2 sm:p-4">
 			{[...Array(SKELETON_STACK_SIZE)].map((_, index) => (
-				<Skeleton className="h-32 w-full" key={String(index)} />
+				<Skeleton className="h-48 w-full" key={String(index)} />
 			))}
 		</div>
 	);
