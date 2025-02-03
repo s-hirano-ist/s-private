7c155d3e195dac11060e5cc13d5a14dde9e83e70
s-hirano-ist <s-hirano-ist@outlook.com>
2025-02-03 21:09:14 +0900

    fix: 🐛 image page size

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const PAGE_NAME = "s-private";
 
-export const PAGE_SIZE = 12;
+export const PAGE_SIZE = 24;
 
 // NOTE: sync with s-contents/update-db.ts
 export const ORIGINAL_IMAGE_PATH = "images/original";
