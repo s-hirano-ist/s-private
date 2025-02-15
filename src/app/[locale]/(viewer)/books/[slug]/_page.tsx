16426aef743d052dcfd71ac22c85e0a6bb715d7a
s-hirano-ist <s-hirano-ist@outlook.com>
2025-02-15 21:06:10 +0900

    fix: 🐛 optimized route test

--- src/app/[locale]/(viewer)/books/[slug]/_page.tsx
+++ src/app/[locale]/(viewer)/books/[slug]/_page.tsx
@@ -1,14 +1,10 @@
 import { ViewerBody } from "@/components/body/viewer-body";
 import { NotFound } from "@/components/card/not-found";
-import { Unauthorized } from "@/components/card/unauthorized";
-import { hasViewerAdminPermission } from "@/features/auth/utils/session";
 import prisma from "@/prisma";
 
 type Props = { slug: string };
 
 export async function SuspensePage({ slug }: Props) {
-	const hasAdminPermission = await hasViewerAdminPermission();
-
 	const decordedSlug = decodeURIComponent(slug);
 
 	const data = await prisma.staticBooks.findUnique({
@@ -18,13 +14,5 @@ export async function SuspensePage({ slug }: Props) {
 	});
 	if (!data) return <NotFound />;
 
-	return (
-		<>
-			{hasAdminPermission ? (
-				<ViewerBody markdown={data.markdown} />
-			) : (
-				<Unauthorized />
-			)}
-		</>
-	);
+	return <ViewerBody markdown={data.markdown} />;
 }
