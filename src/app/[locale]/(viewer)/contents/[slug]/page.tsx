fdf7023acbb51cc9fcf379695422dd5a2f22a763
s-hirano-ist <s-hirano-ist@outlook.com>
2025-02-15 17:53:19 +0900

    refactor: 💡 revert max duration

--- src/app/[locale]/(viewer)/contents/[slug]/page.tsx
+++ src/app/[locale]/(viewer)/contents/[slug]/page.tsx
@@ -5,7 +5,6 @@ import { Suspense } from "react";
 import { SuspensePage } from "./_page";
 
 export const dynamic = "force-dynamic";
-export const maxDuration = 30;
 
 type Params = Promise<{ slug: string }>;
 
