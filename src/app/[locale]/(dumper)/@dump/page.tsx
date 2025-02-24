b0938aa74f3f40de6d4fc57ec18d6e9ab8ed0d81
s-hirano-ist <s-hirano-ist@outlook.com>
2025-02-24 12:18:35 +0900

    fix: 🐛 delete force dynamic

--- src/app/[locale]/(dumper)/@dump/page.tsx
+++ src/app/[locale]/(dumper)/@dump/page.tsx
@@ -2,8 +2,6 @@ import Loading from "@/components/loading";
 import { Suspense } from "react";
 import { SuspensePage } from "./_page";
 
-export const dynamic = "force-dynamic";
-
 export default async function Page() {
 	return (
 		<Suspense fallback={<Loading />}>
--- src/app/[locale]/search/page.tsx
+++ src/app/[locale]/search/page.tsx
@@ -1,7 +1,5 @@
 import { StatusCodeView } from "@/components/card/status-code-view";
 
-export const dynamic = "force-dynamic";
-
 export default function Page() {
 	return <StatusCodeView statusCode="000" />;
 }
