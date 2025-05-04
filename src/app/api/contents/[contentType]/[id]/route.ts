25a57be1265bf3755202c052928706e7360b8b71
s-hirano-ist <s-hirano-ist@outlook.com>
2025-05-04 10:23:25 +0900

    fix: 🐛 route handler auth not working

--- src/app/api/contents/[contentType]/[id]/route.ts
+++ src/app/api/contents/[contentType]/[id]/route.ts
@@ -5,43 +5,48 @@ import {
 } from "@/constants";
 import { env } from "@/env";
 import { NotAllowedError } from "@/error-classes";
-import { hasViewerAdminPermission } from "@/features/auth/utils/session";
+import { auth } from "@/features/auth/utils/auth";
 import { minioClient } from "@/minio";
 import { loggerError } from "@/pino";
 import { redirect } from "next/navigation";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(
-	_: NextRequest,
-	{ params }: { params: Promise<{ contentType: string; id: string }> },
-) {
-	try {
-		const { contentType, id } = await params;
+export const GET = auth(
+	async (
+		request,
+		{ params }: { params: Promise<{ contentType: string; id: string }> },
+	) => {
+		try {
+			const { contentType, id } = await params;
 
-		const hasViewerPermission = await hasViewerAdminPermission();
-		if (!hasViewerPermission) throw new NotAllowedError();
+			if (!request.auth)
+				return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-		const objKey = `${contentType === "thumbnail" ? THUMBNAIL_IMAGE_PATH : ORIGINAL_IMAGE_PATH}/${id}`;
+			if (!request.auth.user.roles.includes("viewer"))
+				throw new NotAllowedError();
 
-		const stream = await minioClient.getObject(env.MINIO_BUCKET_NAME, objKey);
+			const objKey = `${contentType === "thumbnail" ? THUMBNAIL_IMAGE_PATH : ORIGINAL_IMAGE_PATH}/${id}`;
 
-		// eslint-disable-next-line
-		return new Response(stream as any, {
-			headers: {
-				"Content-Type": "image/jpeg",
-				"Cache-Control": "public, max-age=31536000, immutable",
-			},
-		});
-	} catch (error) {
-		loggerError(
-			"unexpected",
-			{
-				caller: "GenerateUrlError",
-				status: 500,
-			},
-			error,
-		);
-		// FIXME: redirect not working
-		redirect(NOT_FOUND_IMAGE_PATH);
-	}
-}
+			const stream = await minioClient.getObject(env.MINIO_BUCKET_NAME, objKey);
+
+			// eslint-disable-next-line
+			return new Response(stream as any, {
+				headers: {
+					"Content-Type": "image/jpeg",
+					"Cache-Control": "public, max-age=31536000, immutable",
+				},
+			});
+		} catch (error) {
+			loggerError(
+				"unexpected",
+				{
+					caller: "GenerateUrlError",
+					status: 500,
+				},
+				error,
+			);
+			// FIXME: redirect not working
+			redirect(NOT_FOUND_IMAGE_PATH);
+		}
+	},
+);
