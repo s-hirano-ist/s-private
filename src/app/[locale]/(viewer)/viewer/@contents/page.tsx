1fc00b75f955ad0ed12d56e6dc024ed8b33976fc
s-hirano-ist <s-hirano-ist@outlook.com>
2025-06-28 15:21:39 +0900

    perf: update performance of tabs

--- src/app/[locale]/(viewer)/viewer/@contents/page.tsx
+++ src/app/[locale]/(viewer)/viewer/@contents/page.tsx
@@ -2,7 +2,18 @@ import { Suspense } from "react";
 import Loading from "@/components/loading";
 import { SuspensePage } from "./_page";
 
-export default async function Page() {
+type Props = {
+	searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function Page({ searchParams }: Props) {
+	const activeTab = searchParams.tab as string;
+
+	// Only render if this tab is active
+	if (activeTab && activeTab !== "contents") {
+		return <div />;
+	}
+
 	return (
 		<Suspense fallback={<Loading />}>
 			<SuspensePage />
