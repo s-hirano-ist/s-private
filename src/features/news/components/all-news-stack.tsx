47b0fb8468338ff8895dc0ae25e1409a5bbedcad
s-hirano-ist <s-hirano-ist@outlook.com>
2025-07-19 18:55:00 +0900

    feat: add news and summary

--- src/features/news/components/all-news-stack.tsx
+++ src/features/news/components/all-news-stack.tsx
@@ -1,15 +1,14 @@
 import { StatusCodeView } from "@/components/card/status-code-view";
-import { CardStack } from "@/components/stack/card-stack";
 import { getStaticNews } from "@/features/viewer/actions/static-news";
 import { loggerError } from "@/pino";
+import { TempStack } from "./temp-stack";
 
 type Props = { page: number };
 
 export async function AllNewsStack({ page }: Props) {
 	try {
 		const news = await getStaticNews(page);
-
-		return <CardStack data={news} showDeleteButton={false} />;
+		return <TempStack data={news} />;
 	} catch (error) {
 		loggerError(
 			"unexpected",
