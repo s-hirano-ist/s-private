95de0bf7cb9eb596d53274c4d9a69cb9e010eb15
s-hirano-ist <s-hirano-ist@outlook.com>
2025-08-03 16:20:23 +0900

    test: fix storybook test

--- src/app/[locale]/search/simple-search/client.stories.tsx
+++ src/app/[locale]/search/simple-search/client.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { fn } from "@storybook/test";
 import { SimpleSearchClient } from "./client";
 
 const meta = {
@@ -9,4 +10,8 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+	args: {
+		searchKnowledge: fn(),
+	},
+};
