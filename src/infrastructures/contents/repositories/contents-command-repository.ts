d28882a4c01ca1b2df7a656ae8ad4c3b7ab4c8e2
s-hirano-ist <s-hirano-ist@outlook.com>
2025-08-15 20:24:20 +0900

    feat: add delete contents

--- src/infrastructures/contents/repositories/contents-command-repository.ts
+++ src/infrastructures/contents/repositories/contents-command-repository.ts
@@ -1,3 +1,4 @@
+import type { Status } from "@/domains/common/entities/common-entity";
 import type { ContentsFormSchema } from "@/domains/contents/entities/contents-entity";
 import type { IContentsCommandRepository } from "@/domains/contents/types";
 import { serverLogger } from "@/o11y/server";
@@ -9,6 +10,18 @@ class ContentsCommandRepository implements IContentsCommandRepository {
 		serverLogger.info(
 			`【CONTENTS】\n\nコンテンツ\ntitle: ${response.title} \nquote: ${response.markdown}\nの登録ができました`,
 			{ caller: "addContents", status: 201, userId: data.userId },
+			{ notify: true },
+		);
+	}
+
+	async deleteById(id: string, userId: string, status: Status): Promise<void> {
+		const data = await prisma.contents.delete({
+			where: { id, userId, status },
+			select: { title: true },
+		});
+		serverLogger.info(
+			`【CONTENTS】\n\n削除\ntitle: ${data.title}`,
+			{ caller: "deleteContents", status: 200, userId },
 			{ notify: true },
 		);
 	}
