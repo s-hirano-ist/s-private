92c4a71196deceedb58c436fc23ae5965e87cb92
s-hirano-ist <s-hirano-ist@outlook.com>
2025-08-19 18:14:40 +0900

    fix: designs of command search

--- src/components/common/features/search/search-card.tsx
+++ src/components/common/features/search/search-card.tsx
@@ -1,18 +1,17 @@
 "use client";
 import { useTranslations } from "next-intl";
+import type { searchContentFromClient } from "@/application-services/search/search-content-from-client";
 import { StatusCodeView } from "@/components/common/display/status/status-code-view";
 import { useSearch } from "@/components/common/hooks/use-search";
 import {
-	Command,
 	CommandEmpty,
 	CommandInput,
 	CommandItem,
 	CommandList,
 } from "@/components/common/ui/command";
 import Loading from "../../display/loading";
-import type { search } from "./search";
 
-type Props = { search: typeof search };
+type Props = { search: typeof searchContentFromClient };
 
 export function SearchCard({ search }: Props) {
 	const t = useTranslations("label");
@@ -26,30 +25,30 @@ export function SearchCard({ search }: Props) {
 	};
 
 	return (
-		<div className="px-2 sm:px-4">
-			<Command className="my-4 rounded-lg border">
-				<CommandInput
-					onValueChange={handleInputChange}
-					placeholder={t("search")}
-					value={searchQuery}
-				/>
-				<CommandList>
-					{searchResults === undefined ||
-					(searchResults?.length === 0 && searchQuery && !isPending) ? (
-						<CommandEmpty>
+		<>
+			<CommandInput
+				onValueChange={handleInputChange}
+				placeholder={t("search")}
+				value={searchQuery}
+			/>
+			<CommandList>
+				{searchResults === undefined ||
+				(searchResults?.length === 0 && searchQuery && !isPending) ? (
+					<CommandEmpty>
+						<div className="flex items-center justify-center">
 							<StatusCodeView statusCode="204" />
-						</CommandEmpty>
-					) : isPending ? (
-						<div className="p-4">
-							<Loading />
 						</div>
-					) : (
-						searchResults?.map((item, index) => (
-							<CommandItem key={String(index)}>{item.title}</CommandItem>
-						))
-					)}
-				</CommandList>
-			</Command>
-		</div>
+					</CommandEmpty>
+				) : isPending ? (
+					<div className="p-4">
+						<Loading />
+					</div>
+				) : (
+					searchResults?.map((item, index) => (
+						<CommandItem key={String(index)}>{item.title}</CommandItem>
+					))
+				)}
+			</CommandList>
+		</>
 	);
 }
--- src/components/common/ui/dialog.tsx
+++ src/components/common/ui/dialog.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Cross2Icon } from "@radix-ui/react-icons";
 import { Dialog as DialogPrimitive } from "radix-ui";
 import * as React from "react";
 
@@ -29,13 +28,16 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const DialogContent = React.forwardRef<
 	React.ElementRef<typeof DialogPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({ className, children, ...props }, ref) => (
+	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
+		fullWidth?: boolean;
+	}
+>(({ className, children, fullWidth = false, ...props }, ref) => (
 	<DialogPortal>
 		<DialogOverlay />
 		<DialogPrimitive.Content
 			className={cn(
-				"fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+				"fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg max-w-4xl",
+				!fullWidth && "max-w-lg",
 				className,
 			)}
 			ref={ref}
