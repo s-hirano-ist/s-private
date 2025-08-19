26bf954f3a86f65322583230b5a152a7e969c803
s-hirano-ist <s-hirano-ist@outlook.com>
2025-08-19 19:23:14 +0900

    fix: search flick bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Noto_Sans_JP } from "next/font/google";
-import { unstable_ViewTransition as ViewTransitions } from "react";
+// FIXME: view transition does not work on search page
+// import { unstable_ViewTransition as ViewTransitions } from "react";
 import { env } from "@/env";
 
 const notoSansJp = Noto_Sans_JP({ subsets: ["latin"] });
@@ -12,24 +13,22 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: ReactNode }>) {
 	return (
-		<ViewTransitions>
+		<html lang="ja" suppressHydrationWarning>
 			{/*https://github.com/pacocoursey/next-themes*/}
-			<html lang="ja" suppressHydrationWarning>
-				<head>
-					{/* https://github.com/aidenybai/react-scan */}
-					{env.NODE_ENV === "development" && (
-						<script
-							async
-							src="https://unpkg.com/react-scan/dist/auto.global.js"
-						/>
-					)}
-				</head>
-				<body className={notoSansJp.className}>
-					{children}
-					<Analytics />
-					<SpeedInsights />
-				</body>
-			</html>
-		</ViewTransitions>
+			<head>
+				{/* https://github.com/aidenybai/react-scan */}
+				{env.NODE_ENV === "development" && (
+					<script
+						async
+						src="https://unpkg.com/react-scan/dist/auto.global.js"
+					/>
+				)}
+			</head>
+			<body className={notoSansJp.className}>
+				{children}
+				<Analytics />
+				<SpeedInsights />
+			</body>
+		</html>
 	);
 }
