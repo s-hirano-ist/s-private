1b8d9cf21eb55c9afc881dd91933beec0a4ec1b1
s-hirano-ist <s-hirano-ist@outlook.com>
2025-08-24 17:27:29 +0900

    test: update storybook test settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -40,6 +41,30 @@ export default defineConfig({
 			include: ["./src/**/*.test.?(c|m)[jt]s?(x)"],
 			tsconfig: "./tsconfig.json",
 		},
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					// The plugin will run tests for the stories defined in your Storybook config
+					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+					storybookTest({ configDir: ".storybook" }),
+				],
+				test: {
+					name: "storybook",
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: "playwright",
+						instances: [
+							{
+								browser: "chromium",
+							},
+						],
+					},
+					setupFiles: [".storybook/vitest.setup.ts"],
+				},
+			},
+		],
 	},
 	resolve: { alias: { "@": "/src" } },
 });
