513f967dc996d1a0ca0f728d7f0d2bb82f143ee2
s-hirano-ist <s-hirano-ist@outlook.com>
2026-02-22 09:05:47 +0900

    perf: delete sentry heavy settings

--- app/instrumentation-client.ts
+++ app/instrumentation-client.ts
@@ -2,34 +2,14 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import {
-	captureConsoleIntegration,
-	captureRouterTransitionStart,
-	init,
-	replayIntegration,
-} from "@sentry/nextjs";
+import { captureRouterTransitionStart, init } from "@sentry/nextjs";
 import { env } from "@/env";
 
 export const onRouterTransitionStart = captureRouterTransitionStart;
 
 init({
 	dsn: env.NEXT_PUBLIC_SENTRY_DSN,
-	// enabled: process.env.NODE_ENV === "production",
-
-	// Add optional integrations for additional features
-	integrations: [replayIntegration(), captureConsoleIntegration()],
-
-	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-	tracesSampleRate: 1,
-
-	// Define how likely Replay events are sampled.
-	// This sets the sample rate to be 10%. You may want this to be 100% while
-	// in development and sample at a lower rate in production
-	replaysSessionSampleRate: 0.1,
-
-	// Define how likely Replay events are sampled when an error occurs.
-	replaysOnErrorSampleRate: 1,
-
-	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	integrations: [],
+	tracesSampleRate: 0.2,
 	debug: false,
 });
