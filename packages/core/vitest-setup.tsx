0bd1b67f72613d91a3ccd8d696dbd2d9be454e11
s-hirano-ist <s-hirano-ist@outlook.com>
2026-02-28 10:31:07 +0900

    fix: vitest mutation test bug

--- packages/core/vitest-setup.tsx
+++ packages/core/vitest-setup.tsx
@@ -1,32 +1,33 @@
 import type { Sharp } from "sharp";
 import { afterEach, beforeEach, vi } from "vitest";
 
+type PartialSharp = Pick<Sharp, "metadata" | "resize" | "toBuffer">;
+
+vi.mock("uuid", () => ({
+	v7: () => {
+		return "01234567-89ab-7def-8123-456789abcd00";
+	},
+}));
+
+vi.mock("sharp", () => {
+	return {
+		__esModule: true,
+		default: vi.fn(() => {
+			const mockSharp: PartialSharp = {
+				metadata: vi.fn().mockResolvedValue({
+					width: 800,
+					height: 600,
+				}),
+				resize: vi.fn().mockReturnThis(),
+				toBuffer: vi.fn().mockResolvedValue(Buffer.from("thumbnail")),
+			};
+			return mockSharp;
+		}),
+	};
+});
+
 afterEach(() => {});
 
 beforeEach(() => {
 	vi.clearAllMocks();
-
-	vi.mock("uuid", () => ({
-		v7: () => {
-			return "01234567-89ab-7def-8123-456789abcd00";
-		},
-	}));
-
-	type PartialSharp = Pick<Sharp, "metadata" | "resize" | "toBuffer">;
-	vi.mock("sharp", () => {
-		return {
-			__esModule: true,
-			default: vi.fn(() => {
-				const mockSharp: PartialSharp = {
-					metadata: vi.fn().mockResolvedValue({
-						width: 800,
-						height: 600,
-					}),
-					resize: vi.fn().mockReturnThis(),
-					toBuffer: vi.fn().mockResolvedValue(Buffer.from("thumbnail")),
-				};
-				return mockSharp;
-			}),
-		};
-	});
 });
--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
+		root: import.meta.dirname,
 		setupFiles: ["./vitest-setup.tsx"],
 		include: ["./**/*.test.?(c|m)[jt]s?(x)"],
 		typecheck: {
