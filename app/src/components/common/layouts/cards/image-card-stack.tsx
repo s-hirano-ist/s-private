8ea517db81c79dc36ab5100af602568b47745b82
Sola <s-hirano-ist@outlook.com>
2026-09-21 17:22:25 +0900

    fix: more designs

--- app/src/components/common/layouts/cards/image-card-stack.tsx
+++ app/src/components/common/layouts/cards/image-card-stack.tsx
@@ -21,7 +21,7 @@ export function ImageCardStack({
 	return (
 		<BaseCardStackWrapper
 			deleteAction={deleteAction}
-			gridClassName="my-2 grid grid-cols-2 items-stretch gap-4 sm:grid-cols-3 lg:grid-cols-4"
+			gridClassName="grid grid-cols-2 items-stretch gap-4 sm:grid-cols-3 lg:grid-cols-4"
 			initial={initial}
 			loadMoreAction={loadMoreAction}
 		>
